5e8f01d50a43d91894ff9993129d325fd0eee80b
fatih255 <fatih969692@gmail.com>
2022-10-04 20:39:49 +0300

    generated

--- components/modals/layouts/DefaultLayout.tsx
+++ components/modals/layouts/DefaultLayout.tsx
@@ -21,7 +21,7 @@ export default function DefaultLayout({ children }: layoutTypes) {
     return (
         <div id="layout"
             className={`default ${className} ${size} ${position}`}>
-            <button className="no-w-full absolute top-0 right-0 hover:scale-110  trans-300 m-[6%]" onClick={closeModalHandler}>
+            <button id="close-modal-btn" className="no-w-full absolute top-0 right-0 hover:scale-110  trans-300 m-[6%]" onClick={closeModalHandler}>
                 <CloseModalIcon className="svg-gray-600" />
             </button>
             {children}
--- lib/generateCode.ts
+++ lib/generateCode.ts
@@ -29,22 +29,74 @@ export default function generateCode(): string {
     //3.Stage: Add the hostname to the beginning of the image's source url string
     modalElement_cloned.querySelectorAll("img").forEach(img => img.src = img.src)
 
+    //4.Stage: Import event listeners
+    // modalElement_cloned.
+
     // Final Stage: create code between script tags
     const generatedCode = `
     <script>
-      document.body.innerHTML = '${modalElement_cloned.outerHTML}';
-      var cssId = "modalCardCSS"; 
-      var head = document.getElementsByTagName("head")[0];
-      var link = document.createElement("link");
-      link.id = cssId;
-      link.rel = "stylesheet";
-      link.type = "text/css";
-      link.href ="https://633a7bffa39a490df7495584--leafy-mermaid-eb53cb.netlify.app/_next/static/css/c51015471aa6e4bc.css";
-      link.media = "all";
-      head.appendChild(link);
+        document.body.innerHTML = '${modalElement_cloned.outerHTML}';
+        var cssId = "modalCardCSS"; 
+        var head = document.getElementsByTagName("head")[0];
+        var link = document.createElement("link");
+        link.id = cssId;
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href ="https://leafy-mermaid-eb53cb.netlify.app/_next/static/css/1c7e9cfb1ac0f201.css";
+        link.media = "all";
+        head.appendChild(link);
+
+        const modalElement = document.getElementById('layout')
+        const modalCloseButton=document.getElementById('close-modal-btn')
+        modalCloseButton.addEventListener("click",closeModalAction)
+
+        function openModalAction(){
+            modalElement.classList.remove('close');
+            modalElement.classList.add('open');
+        }
+        function closeModalAction(){
+                modalElement.classList.remove('open');
+                modalElement.classList.add('close');
+        }
+
+        // event functions
+        // scroll percentage 
+        function scrollEventOnDocument(value){
+           
+                let scrollTop = window.scrollY;
+                let docHeight = document.body.offsetHeight;
+                let winHeight = window.innerHeight;
+                let scrollPercent = scrollTop / (docHeight - winHeight);
+                let scrollPercentRounded = Math.round(scrollPercent * 100);
+    
+                scrollPercentRounded < Number(value) && closeModal()
+                if (scrollPercentRounded === Number(value) && modalElement.classList.contains('close')) {
+                    openModalAction()
+                } 
+        } 
+
+        // x Second Time Out x after show modal
+        function xSecondTimeOut(value){
+            timer = Number(value) > 0 && setTimeout(() => {
+                openModalAction()
+            }, Number(value) * 1000);
+        }
+
+        //exit indent targeting show modal
+        function exitIntentTargetting(e){
+            if (!e.relatedTarget && modalElement.classList.contains('close')) {
+                openModalAction()
+            }
+        };
+
+        ${activeSettingsValues.afterPercentageScroll ? `window.addEventListener("scroll", scrollEventOnDocument(${activeSettingsValues.afterPercentageScroll}));` : ''}
+        ${activeSettingsValues.exitIntentTargetting ? `document.addEventListener("mouseout" , exitIntentTargetting(${activeSettingsValues.exitIntentTargetting}));` : ''}
+        ${activeSettingsValues.afterXSeconds ? `xSecondTimeOut(${activeSettingsValues.afterXSeconds});` : ''}
+        
+       
+
     </script>
     `
-
 
 
     return generatedCode
